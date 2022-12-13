6f51df4de75eaa1c864d2371323c70ca1ba7ab39
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-13 12:23:33 +0300

    Add files via upload

---
+++ VKI.java
@@ -0,0 +1,14 @@
+package Baslangic;
+import java.util.Scanner;
+public class VKI {
+    public static void main(String[] args) {
+        Scanner veri = new Scanner(System.in);
+        double kilo, boy, VKI;
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz : ");
+        boy = veri.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = veri.nextDouble();
+        VKI = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndexiniz : " +VKI);
+    }
+}
